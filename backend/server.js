2ff6debf6de982033cfc81546c7c361f570cc2cc
Nakulesh Jayakrishnan <nakuleshj1998@gmail.com>
2020-09-25 17:48:50 +0530

    fix build issues

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const bodyParser = require('body-parser');
 const app=express();
+const path = require('path')
 const mongoose=require('mongoose');
 const cors=require('cors');
 const port=process.env.PORT || 5000;
@@ -13,6 +14,10 @@ const projectRouter=require('./routes/project');
 app.use('/authenticate',authenticationRouter);
 app.use('/bug',bugRouter);
 app.use('/project',projectRouter);
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
+})
 const uri=process.env.ATLAS_URI;
 mongoose.connect(uri,{useNewUrlParser: true, useCreateIndex:true,useUnifiedTopology: true});
 // const excercisesRouter=require('./routes/exercises');
